ba5c8e45f0a65b561b19383dbf2f3b5d055213aa
楊健麟 <derek.y@tc-gaming.co>
2017-10-16 14:59:33 +0800

    Initial Commit

---
+++ TestPhotoView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestPhotoView
+//
+//  Created by DerekYang on 2017/10/16.
+//  Copyright © 2017年 LBD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
